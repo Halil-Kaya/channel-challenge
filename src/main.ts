dc4c70cbb7c898b091e8f5d36c7e7b8caacda5c8
Halil Kaya <halilkaya@teknasyon.com>
2023-06-18 10:28:10 +0300

    feat: Add global validation pipe.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true
+        })
+    );
     await app.listen(3050);
+    return app;
 }
-bootstrap();
+bootstrap()
+    .then(async (app) => {
+        console.log(`App is running on: ${await app.getUrl()}`);
+    })
+    .catch((error: Error) => console.error(error.message));
