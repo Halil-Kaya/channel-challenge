ae539e60316b11b3d99ea2f856767f6620b9790f
Halil Kaya <halilkaya@teknasyon.com>
2023-06-29 02:22:02 +0300

    document(swagger): Add swagger package.

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './core/interceptor';
 import { AllExceptionFilter } from './core/filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder().setTitle('Nestjs base project').setVersion('1.0').build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
